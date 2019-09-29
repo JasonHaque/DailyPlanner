8a1451b6fd86420f3d2f8d622e1948a8f18967a5
jason <zahinjason220434@gmail.com>
2019-09-29 13:15:47 +0600

    Added basic field checking for signup

--- app/src/main/java/com/example/dailyplanner/SignUpActivity.java
+++ app/src/main/java/com/example/dailyplanner/SignUpActivity.java
@@ -2,14 +2,17 @@ package com.example.dailyplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private EditText signupMail,signupPassword,confirmPassword;
+    private ProgressDialog progressDialog;
     private Button signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,7 @@ public class SignUpActivity extends AppCompatActivity {
         signupPassword=findViewById(R.id.signup_password);
         confirmPassword=findViewById(R.id.confirm_signup_password);
         signUp=findViewById(R.id.signup_button);
+        progressDialog=new ProgressDialog(this);
     }
 
     void bindListeners(){
@@ -31,7 +35,23 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                initiateSignUp();
+
             }
         });
+    }
+
+    void initiateSignUp(){
+        String email=signupMail.getText().toString();
+        String password=signupPassword.getText().toString();
+        String confirm = confirmPassword.getText().toString();
+        if(email.isEmpty() || password.isEmpty() || confirm.isEmpty()){
+            Toast.makeText(SignUpActivity.this,"Fill up the fields properly", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(!password.equals(confirm)){
+            Toast.makeText(SignUpActivity.this,"Passwords do not match", Toast.LENGTH_LONG).show();
+            return;
+        }
     }
 }
