9b9733a34b24a211b752124dddcf1086c2be5399
jason <zahinjason220434@gmail.com>
2019-09-29 23:37:35 +0600

    Added user view

--- app/src/main/java/com/example/dailyplanner/ProfileActivity.java
+++ app/src/main/java/com/example/dailyplanner/ProfileActivity.java
@@ -6,24 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+
+import static com.example.dailyplanner.LogInActivity.userID;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private Button logout;
     private FirebaseAuth firebaseAuth;
+    private TextView userView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         bindWidgets();
         bindListeners();
+        userView.setText("Welcome User"+userID);
     }
 
     void bindWidgets(){
         logout=findViewById(R.id.log_out);
         firebaseAuth=FirebaseAuth.getInstance();
+        userView=findViewById(R.id.user_view);
+
     }
     void bindListeners(){
         logout.setOnClickListener(new View.OnClickListener() {
