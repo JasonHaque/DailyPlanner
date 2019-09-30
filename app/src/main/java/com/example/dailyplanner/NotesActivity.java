de6bcfb58e647aacb77ce3c3b04d2f1d30921972
afiatamanna06 <afiatamanna06@gmail.com>
2019-09-30 21:11:26 +0600

    Notes activity layout design updated

--- app/src/main/java/com/example/dailyplanner/NotesActivity.java
+++ app/src/main/java/com/example/dailyplanner/NotesActivity.java
@@ -6,10 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class NotesActivity extends AppCompatActivity {
 
-    private Button linkToProfile,newNote,noteHistory,schedule,seeSchedule;
+    private ImageButton linkToProfile,newNote,noteHistory,schedule,seeSchedule;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
