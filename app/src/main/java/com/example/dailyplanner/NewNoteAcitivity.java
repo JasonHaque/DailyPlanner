dde91b8d44e2ab4c2fbc3ad6c0c86db7fbddad63
jason <zahinjason220434@gmail.com>
2019-09-30 12:20:43 +0600

    modified New Note

--- app/src/main/java/com/example/dailyplanner/NewNoteAcitivity.java
+++ app/src/main/java/com/example/dailyplanner/NewNoteAcitivity.java
@@ -3,9 +3,11 @@ package com.example.dailyplanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class NewNoteAcitivity extends AppCompatActivity {
 
@@ -29,6 +31,19 @@ public class NewNoteAcitivity extends AppCompatActivity {
     }
 
     private void bindListeners(){
-
+        setNoteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nameOfNote =setNoteName.getText().toString();
+                if(nameOfNote.isEmpty()){
+                    Toast.makeText(NewNoteAcitivity.this,"Enter a note name",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                noteName.setText(nameOfNote);
+                noteName.setVisibility(View.VISIBLE);
+                setNoteButton.setVisibility(View.INVISIBLE);
+                setNoteName.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
