182881aa0cb98dccecefaf2a4df3e62693c7861f
jason <zahinjason220434@gmail.com>
2019-09-30 09:50:18 +0600

    Notes Activity Layout changed

--- app/src/main/java/com/example/dailyplanner/NotesActivity.java
+++ app/src/main/java/com/example/dailyplanner/NotesActivity.java
@@ -2,13 +2,32 @@ package com.example.dailyplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NotesActivity extends AppCompatActivity {
 
+    private Button linkToProfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+
+        bindWidgets();
+        bindListeners();
+    }
+    private void bindWidgets()
+    {
+        linkToProfile=findViewById(R.id.link_to_profile);
+    }
+    private void bindListeners(){
+        linkToProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(NotesActivity.this,ProfileActivity.class));
+            }
+        });
     }
 }
