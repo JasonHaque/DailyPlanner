2ea473a09ace89b14c2aa642cd14d413f0a7846c
jason <zahinjason220434@gmail.com>
2019-10-01 22:24:05 +0600

    Added progress Dialpog

--- app/src/main/java/com/example/dailyplanner/NewNoteAcitivity.java
+++ app/src/main/java/com/example/dailyplanner/NewNoteAcitivity.java
@@ -1,7 +1,9 @@
 package com.example.dailyplanner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +12,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -22,6 +25,7 @@ public class NewNoteAcitivity extends AppCompatActivity {
     private EditText note,setNoteName;
     private TextView noteName;
     private DatabaseReference dref;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,7 @@ public class NewNoteAcitivity extends AppCompatActivity {
         setNoteName=findViewById(R.id.set_note_name);
         noteName=findViewById(R.id.note_name);
         dref= FirebaseDatabase.getInstance().getReference();
+        progressDialog=new ProgressDialog(this);
     }
 
     private void bindListeners(){
@@ -67,11 +72,21 @@ public class NewNoteAcitivity extends AppCompatActivity {
                 String noteName = setNoteName.getText().toString().trim();
                 String noteContent=note.getText().toString();
                 NewNote userNote = new NewNote(noteName,noteContent);
+                progressDialog.setTitle("Saving");
+                progressDialog.show();
                 dref.child(userID).child("Notes").setValue(userNote).addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
+                        progressDialog.dismiss();
                         Toast.makeText(NewNoteAcitivity.this,"Success",Toast.LENGTH_LONG).show();
 
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Toast.makeText(NewNoteAcitivity.this,"Failed",Toast.LENGTH_LONG).show();
                     }
                 });
             }
