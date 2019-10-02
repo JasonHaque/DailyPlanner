b45bc141a1410fd44bd70e8e597321c42ac8f46f
jason <zahinjason220434@gmail.com>
2019-10-02 09:28:36 +0600

    Added new widgets

--- app/src/main/java/com/example/dailyplanner/SuccessfulNote.java
+++ app/src/main/java/com/example/dailyplanner/SuccessfulNote.java
@@ -3,12 +3,25 @@ package com.example.dailyplanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SuccessfulNote extends AppCompatActivity {
-
+    private TextView successfulNoteName,successfulNoteContent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_successful_note);
+        bindWidgets();
+        bindListeners();
+
+    }
+
+    private void bindWidgets(){
+        successfulNoteName=findViewById(R.id.successful_note_name);
+        successfulNoteContent=findViewById(R.id.successful_note_content);
+    }
+
+    private void bindListeners(){
+
     }
 }
