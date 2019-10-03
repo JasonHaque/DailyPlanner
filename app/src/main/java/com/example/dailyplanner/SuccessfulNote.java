e8244c732d2c66d6e583f2d75d67c80bcc8bb920
afiatamanna06 <afiatamanna06@gmail.com>
2019-10-03 23:57:41 +0600

    Successful note activity layout changed

--- app/src/main/java/com/example/dailyplanner/SuccessfulNote.java
+++ app/src/main/java/com/example/dailyplanner/SuccessfulNote.java
@@ -2,13 +2,17 @@ package com.example.dailyplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import static com.example.dailyplanner.NewNoteActivity.snote;
 
 public class SuccessfulNote extends AppCompatActivity {
     private TextView successfulNoteName,successfulNoteContent;
+    private Button back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,10 +27,17 @@ public class SuccessfulNote extends AppCompatActivity {
     private void bindWidgets(){
         successfulNoteName=findViewById(R.id.successful_note_name);
         successfulNoteContent=findViewById(R.id.successful_note_content);
+        back=findViewById(R.id.backn_button);
         //dref=FirebaseDatabase.getInstance().getReference().child(userID).child(sendNoteName+userID);
     }
 
     private void bindListeners(){
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SuccessfulNote.this,NotesActivity.class));
+            }
+        });
 
     }
 }
