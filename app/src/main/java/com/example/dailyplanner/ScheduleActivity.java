d667716a6a87d4d666c05e85ca8e675b9c84b8b9
jason <zahinjason220434@gmail.com>
2019-10-05 10:12:36 +0600

    set up listeners for schedule

--- app/src/main/java/com/example/dailyplanner/ScheduleActivity.java
+++ app/src/main/java/com/example/dailyplanner/ScheduleActivity.java
@@ -3,12 +3,35 @@ package com.example.dailyplanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ScheduleActivity extends AppCompatActivity {
 
+    private EditText taskName,taskTime;
+    private Button setTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+        bindWidgets();
+        bindListeners();
+    }
+
+    private void bindWidgets(){
+        taskName=findViewById(R.id.schedule_item);
+        taskTime=findViewById(R.id.schedule_time);
+        setTask=findViewById(R.id.set_schedule);
+    }
+    private void bindListeners(){
+
+        setTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: Implement onclick
+            }
+        });
+
     }
 }
