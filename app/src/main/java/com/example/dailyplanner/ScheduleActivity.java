309ffb0c29783927f8337211e981bcd44e27c2c0
jason <zahinjason220434@gmail.com>
2019-10-05 12:21:38 +0600

    Set up empty field check

--- app/src/main/java/com/example/dailyplanner/ScheduleActivity.java
+++ app/src/main/java/com/example/dailyplanner/ScheduleActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ScheduleActivity extends AppCompatActivity {
 
@@ -29,9 +30,20 @@ public class ScheduleActivity extends AppCompatActivity {
         setTask.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO: Implement onclick
+                String name=taskName.getText().toString();
+                String time=taskTime.getText().toString();
+                if(name.isEmpty() || time.isEmpty()){
+                    Toast.makeText(ScheduleActivity.this,"Fill these up properly",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                setupTask(name,time);
+
             }
         });
+
+    }
+
+    private void setupTask(String name,String time){
 
     }
 }
