9b8cae024ffed05d9454116f7ac197b6267cad35
jason <zahinjason220434@gmail.com>
2019-10-05 12:41:05 +0600

    Added Task to database

--- app/src/main/java/com/example/dailyplanner/ScheduleActivity.java
+++ app/src/main/java/com/example/dailyplanner/ScheduleActivity.java
@@ -2,16 +2,25 @@ package com.example.dailyplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import static com.example.dailyplanner.LogInActivity.userID;
+
 public class ScheduleActivity extends AppCompatActivity {
 
     private EditText taskName,taskTime;
     private Button setTask;
+    private DatabaseReference dref;
+    private ProgressDialog progressDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +33,8 @@ public class ScheduleActivity extends AppCompatActivity {
         taskName=findViewById(R.id.schedule_item);
         taskTime=findViewById(R.id.schedule_time);
         setTask=findViewById(R.id.set_schedule);
+        dref= FirebaseDatabase.getInstance().getReference();
+        progressDialog=new ProgressDialog(this);
     }
     private void bindListeners(){
 
@@ -44,6 +55,15 @@ public class ScheduleActivity extends AppCompatActivity {
     }
 
     private void setupTask(String name,String time){
-
+        Task newTask=new Task(name,time);
+        progressDialog.setTitle("Saving");
+        progressDialog.show();
+        dref.child(userID).child("Schedule").child(name+time).setValue(newTask).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                progressDialog.dismiss();
+                Toast.makeText(ScheduleActivity.this,"Success",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/dailyplanner/Task.java
@@ -0,0 +1,14 @@
+package com.example.dailyplanner;
+
+public class Task {
+    public String nameTask,timeTask;
+
+    public Task(){
+
+    }
+
+    public Task(String nameTask, String timeTask) {
+        this.nameTask = nameTask;
+        this.timeTask = timeTask;
+    }
+}
