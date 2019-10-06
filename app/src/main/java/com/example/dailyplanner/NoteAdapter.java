ff102f1c68c59c3534397ca0c80c263f0dc2eca1
jason <zahinjason220434@gmail.com>
2019-10-06 19:42:57 +0600

    Static recyclerview added

--- app/src/main/java/com/example/dailyplanner/NoteAdapter.java
+++ app/src/main/java/com/example/dailyplanner/NoteAdapter.java
@@ -1,4 +1,58 @@
 package com.example.dailyplanner;
 
-public class NoteAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteHolder> {
+
+    private TextView nameNote,ContentNote;
+    private Context context;
+    private ArrayList<NewNote> notes;
+
+    public NoteAdapter(Context context, ArrayList<NewNote> planets) {
+            this.context = context;
+            this.notes = planets;
+        }
+
+    @NonNull
+    @Override
+    public NoteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.notes, parent, false);
+        return new NoteHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoteHolder holder, int position) {
+        NewNote note = notes.get(position);
+        holder.setDetails(note);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public class NoteHolder extends RecyclerView.ViewHolder{
+        public NoteHolder(View itemView){
+            super(itemView);
+            nameNote=itemView.findViewById(R.id.recycler_note_name);
+            ContentNote=itemView.findViewById(R.id.recycler_note_content);
+        }
+
+        public void setDetails(NewNote note) {
+
+            nameNote.setText(note.getName());
+            ContentNote.setText(note.getContent());
+
+        }
+    }
 }
--- app/src/main/java/com/example/dailyplanner/NoteHistoryActivity.java
+++ app/src/main/java/com/example/dailyplanner/NoteHistoryActivity.java
@@ -1,14 +1,37 @@
 package com.example.dailyplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class NoteHistoryActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+    private NoteAdapter adapter;
+    private ArrayList<NewNote> noteArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_history);
+
+        recyclerView=findViewById(R.id.recyclerViewNotes);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        noteArrayList = new ArrayList<>();
+        adapter = new NoteAdapter(this, noteArrayList);
+        recyclerView.setAdapter(adapter);
+        createlistData();
+
+    }
+    private void createlistData(){
+        NewNote note =new NewNote("bla bla ","bla bla bla bla");
+        noteArrayList.add(note);
+        note =new NewNote("la bla ","la bla bla");
+        noteArrayList.add(note);
+        note =new NewNote("bla bla ","bla bla bla bla");
+        noteArrayList.add(note);
     }
 }
