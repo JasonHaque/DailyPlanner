4ab983cd2010547458f53dd6d21d7d7649dcd73c
afiatamanna06 <afiatamanna06@gmail.com>
2019-11-06 14:54:49 +0600

    Onbackpressed method added at Notes activity

--- app/src/main/java/com/example/dailyplanner/NotesActivity.java
+++ app/src/main/java/com/example/dailyplanner/NotesActivity.java
@@ -121,4 +121,11 @@ public class NotesActivity extends AppCompatActivity {
         }
         return false;
     }
+    @Override
+    public void onBackPressed(){
+        Intent a = new Intent(Intent.ACTION_MAIN);
+        a.addCategory(Intent.CATEGORY_HOME);
+        a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(a);
+    }
 }
